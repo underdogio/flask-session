f54b73a26ae743d3a57205a3b836016e42dcd374
Todd Wolfson <todd@twolfson.com>
2015-08-04 19:51:47 -0500

    Added back tests

--- test_session.py
+++ test_session.py
@@ -44,93 +44,93 @@ class FlaskSessionTestCase(unittest.TestCase):
         c.post('/delete')
 
 
-    # def test_memcached_session(self):
-    #     app = flask.Flask(__name__)
-    #     app.config['SESSION_TYPE'] = 'memcached'
-    #     Session(app)
-    #     @app.route('/set', methods=['POST'])
-    #     def set():
-    #         flask.session['value'] = flask.request.form['value']
-    #         return 'value set'
-    #     @app.route('/get')
-    #     def get():
-    #         return flask.session['value']
-    #     @app.route('/delete', methods=['POST'])
-    #     def delete():
-    #         del flask.session['value']
-    #         return 'value deleted'
+    def test_memcached_session(self):
+        app = flask.Flask(__name__)
+        app.config['SESSION_TYPE'] = 'memcached'
+        Session(app)
+        @app.route('/set', methods=['POST'])
+        def set():
+            flask.session['value'] = flask.request.form['value']
+            return 'value set'
+        @app.route('/get')
+        def get():
+            return flask.session['value']
+        @app.route('/delete', methods=['POST'])
+        def delete():
+            del flask.session['value']
+            return 'value deleted'
 
-    #     c = app.test_client()
-    #     self.assertEqual(c.post('/set', data={'value': '42'}).data, b'value set')
-    #     self.assertEqual(c.get('/get').data, b'42')
-    #     c.post('/delete')
+        c = app.test_client()
+        self.assertEqual(c.post('/set', data={'value': '42'}).data, b'value set')
+        self.assertEqual(c.get('/get').data, b'42')
+        c.post('/delete')
 
 
-    # def test_filesystem_session(self):
-    #     app = flask.Flask(__name__)
-    #     app.config['SESSION_TYPE'] = 'filesystem'
-    #     app.config['SESSION_FILE_DIR'] = tempfile.gettempdir()
-    #     Session(app)
-    #     @app.route('/set', methods=['POST'])
-    #     def set():
-    #         flask.session['value'] = flask.request.form['value']
-    #         return 'value set'
-    #     @app.route('/get')
-    #     def get():
-    #         return flask.session['value']
-    #     @app.route('/delete', methods=['POST'])
-    #     def delete():
-    #         del flask.session['value']
-    #         return 'value deleted'
+    def test_filesystem_session(self):
+        app = flask.Flask(__name__)
+        app.config['SESSION_TYPE'] = 'filesystem'
+        app.config['SESSION_FILE_DIR'] = tempfile.gettempdir()
+        Session(app)
+        @app.route('/set', methods=['POST'])
+        def set():
+            flask.session['value'] = flask.request.form['value']
+            return 'value set'
+        @app.route('/get')
+        def get():
+            return flask.session['value']
+        @app.route('/delete', methods=['POST'])
+        def delete():
+            del flask.session['value']
+            return 'value deleted'
 
-    #     c = app.test_client()
-    #     self.assertEqual(c.post('/set', data={'value': '42'}).data, b'value set')
-    #     self.assertEqual(c.get('/get').data, b'42')
-    #     c.post('/delete')
+        c = app.test_client()
+        self.assertEqual(c.post('/set', data={'value': '42'}).data, b'value set')
+        self.assertEqual(c.get('/get').data, b'42')
+        c.post('/delete')
 
-    # def test_mongodb_session(self):
-    #     app = flask.Flask(__name__)
-    #     app.config['SESSION_TYPE'] = 'mongodb'
-    #     Session(app)
-    #     @app.route('/set', methods=['POST'])
-    #     def set():
-    #         flask.session['value'] = flask.request.form['value']
-    #         return 'value set'
-    #     @app.route('/get')
-    #     def get():
-    #         return flask.session['value']
-    #     @app.route('/delete', methods=['POST'])
-    #     def delete():
-    #         del flask.session['value']
-    #         return 'value deleted'
+    def test_mongodb_session(self):
+        app = flask.Flask(__name__)
+        app.config['SESSION_TYPE'] = 'mongodb'
+        Session(app)
+        @app.route('/set', methods=['POST'])
+        def set():
+            flask.session['value'] = flask.request.form['value']
+            return 'value set'
+        @app.route('/get')
+        def get():
+            return flask.session['value']
+        @app.route('/delete', methods=['POST'])
+        def delete():
+            del flask.session['value']
+            return 'value deleted'
 
-    #     c = app.test_client()
-    #     self.assertEqual(c.post('/set', data={'value': '42'}).data, b'value set')
-    #     self.assertEqual(c.get('/get').data, b'42')
-    #     c.post('/delete')
+        c = app.test_client()
+        self.assertEqual(c.post('/set', data={'value': '42'}).data, b'value set')
+        self.assertEqual(c.get('/get').data, b'42')
+        c.post('/delete')
 
-    # def test_flasksqlalchemy_session(self):
-    #     app = flask.Flask(__name__)
-    #     app.debug = True
-    #     app.config['SESSION_TYPE'] = 'sqlalchemy'
-    #     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
-    #     Session(app)
-    #     @app.route('/set', methods=['POST'])
-    #     def set():
-    #         flask.session['value'] = flask.request.form['value']
-    #         return 'value set'
-    #     @app.route('/get')
-    #     def get():
-    #         return flask.session['value']
-    #     @app.route('/delete', methods=['POST'])
-    #     def delete():
-    #         del flask.session['value']
-    #         return 'value deleted'
+    def test_flasksqlalchemy_session(self):
+        app = flask.Flask(__name__)
+        app.debug = True
+        app.config['SESSION_TYPE'] = 'sqlalchemy'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+        Session(app)
+        @app.route('/set', methods=['POST'])
+        def set():
+            flask.session['value'] = flask.request.form['value']
+            return 'value set'
+        @app.route('/get')
+        def get():
+            return flask.session['value']
+        @app.route('/delete', methods=['POST'])
+        def delete():
+            del flask.session['value']
+            return 'value deleted'
 
-    #     c = app.test_client()
-    #     self.assertEqual(c.post('/set', data={'value': '42'}).data, b'value '
-    #                                                                 b'set')
-    #     self.assertEqual(c.get('/get').data, b'42')
+        c = app.test_client()
+        self.assertEqual(c.post('/set', data={'value': '42'}).data, b'value '
+                                                                    b'set')
+        self.assertEqual(c.get('/get').data, b'42')
         c.post('/delete')
 
     def test_session_use_signer(self):
